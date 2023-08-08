599ca17cf5c04151483ab408eb04f35c7787ae22
Sufiyan055 <sufiyan007882@gmail.com>
2023-08-08 21:12:52 +0530

    Search bar added and Maze tv API is used and console the data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,30 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [inputValue, setInputValue] = useState("");
+  const [searchStr, setSearchStr] = useState("");
 
-  //console.log(inputValue);
+  //console.log(searchStr);
   /* Here i learn about data binding 1 way and 2 way */
-  const onInputChange = (ev) => {
-    setInputValue(ev.target.value);
+  const onSearchInputChange = (ev) => {
+    setSearchStr(ev.target.value);
   };
+
+  const onSearch = async (ev) => {
+    ev.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = await response.json();
+    console.log(body);
+  };
+
   return (
     <>
-      <div>{inputValue}</div>
-      <button
-        type="button"
-        onClick={() => {
-          setInputValue("Sufiyan");
-        }}
-      >
-        Update to random
-      </button>
-      <input type="text" value={inputValue} onChange={onInputChange} />
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
+      </form>
     </>
   );
 };
